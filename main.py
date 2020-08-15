582b03bdab2118f876decfe86430bd633476ecb4
nastine <nastine928@gmail.com>
2020-08-15 20:42:51 +0300

    Initial comit

---
+++ main.py
@@ -0,0 +1,10 @@
+from modules.vk_groups import write_to_db
+import schedule
+import time
+
+if __name__ == "__main__":
+    schedule.every().day.at("10:30").do(write_to_db)
+    schedule.every().minute.at(":17").do(write_to_db)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
